7bea71c536b7139d1f1ee08eec6fe9ef595ec346
ActiveConclusion <activeconclusioner@gmail.com>
2021-01-24 16:01:14 +0200

    Додано коментарі у config.py

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
+# директорія для збереження даних
 DATA_DIR = "data"
+# назва файлу даних про висунутих кандидатів
 ALL_CANDIDATES_RAW_FILE_CSV = "01_01_all_candidates.csv"
+# назва файлу даних про обраних кандидатів
 ELECTED_CANDIDATES_RAW_FILE_CSV = "01_02_elected_candidates.csv"
+# назва файлу зведених даних про обраних та висунутих кандидатів
 MERGED_FILE_CSV = "02_01_merged_candidates.csv"
+# назва файлу даних про висунутих і обраних кандидатів по партіям, регіонам та радам
 AGGREGATED_FILE_CSV = "03_01_aggregated_data.csv"
 
 CANDIDATES_RAW_FILE_CSV_PATH = {
@@ -13,5 +18,9 @@ CANDIDATES_RAW_FILE_CSV_PATH = {
 MERGED_FILE_CSV_PATH = Path(DATA_DIR, MERGED_FILE_CSV)
 AGGREGATED_FILE_CSV_PATH = Path(DATA_DIR, AGGREGATED_FILE_CSV)
 
+# Регіони для опрацювання.
+# Якщо поставити значення None, то будуть опрацьовані всі можливі регіони
 REGIONS = ["Вінницька область", "Волинська область", "Дніпропетровська область"]
+# Типи рад для опрацювання.
+# Можливі опції: "Обласні", "Міські", "Районні", "Районні у містах"
 TYPES_OF_COUNCILS = ["Міські"]
